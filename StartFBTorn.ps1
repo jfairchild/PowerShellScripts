f25f05254642bbb0311478ff70d4f0c04a3d7f76
Jeff Fairchild <jfairchild@groupcommerce.com>
2013-05-14 14:27:10 -0400

    firefox powershell script

---
+++ StartFBTorn.ps1
@@ -0,0 +1,6 @@
+﻿$ff = Get-Process firefox
+if($ff -ne $null)
+{
+    Stop-Process -Id $ff.Id
+}
+Start-Process 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -ArgumentList "-new-window http://facebook.torn.com"
