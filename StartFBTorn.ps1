5e298e1fee978793753e5b042ed6b11c460222b2
Jeff Fairchild <jfairchild@users.noreply.github.com>
2018-02-06 07:59:44 -0500

    Update StartFBTorn.ps1

--- StartFBTorn.ps1
+++ StartFBTorn.ps1
@@ -3,4 +3,4 @@ if($ff -ne $null)
 {
     Stop-Process -Id $ff.Id
 }
-Start-Process 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -ArgumentList "-new-window http://facebook.torn.com"
+Start-Process 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -ArgumentList "-new-window https://facebook.torn.com"
